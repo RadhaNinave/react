026e6ab2638e34f9e708fff2759c7a0e3b517a0f
RadhaNinave <101126042+RadhaNinave@users.noreply.github.com>
2022-12-30 17:04:02 +0530

    Add files via upload

--- Expenses.js
+++ Expenses.js
@@ -11,40 +11,37 @@ function Expenses(props) {
   const filteredExpenses = props.items.filter(expense =>{
     return expense.date.getFullYear().toString()===filteredYear;
   });
+  let expensesContent = <p>No expenses found.</p>;
+  let expenseLength=<p>Only single Expense here PLease add more</p>
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+  if(expensesContent>0)
+  {
+    expenseLength =filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
   return (
     <Card className="expenses">
       <ExpenseFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
-      />
-      {filteredExpenses.map((expense) => 
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      )}
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
+      /> 
+    {expenseLength}
+      {expensesContent}
     </Card>
   );
 }
