68a106e5e24e1b5709f4ce5845098c53a69abdc4
RadhaNinave <101126042+RadhaNinave@users.noreply.github.com>
2022-12-30 14:51:08 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,63 +1,67 @@
-import "./App.css";
-
-import ExpenseItem from "./component/ExpenseItem";
+import NewExpense from "./component/NewExpenses/NewExpense";
+//import ExpenseItem from "./component/Expenses/ExpenseItem";
+import Expenses from "./component/Expenses/Expenses";
+//import Expenses from "./component/Expenses/Expenses";
 function App() {
-  const expenses = [
+  const expenses=[
     {
-      title: "Toilet Paper",
+      
+      title: 'Toilet Paper',
       amount: 94.12,
       date: new Date(2020, 7, 14),
-      ExpenseLocation: "Home",
     },
+    {  title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
     {
-      title: "New TV",
-      amount: 799.49,
-      date: new Date(2021, 2, 12),
-      ExpenseLocation: "Appliences",
-    },
-    {
-      title: "Car Insurance",
+      
+      title: 'Car Insurance',
       amount: 294.67,
       date: new Date(2021, 2, 28),
-      ExpenseLocation: "CanvasRenderingContext2D",
     },
     {
-      title: "New Desk (Wooden)",
+      
+      title: 'New Desk (Wooden)',
       amount: 450,
       date: new Date(2021, 5, 12),
-      ExpenseLocation: "Laptop",
     },
   ];
-
+  const addExpenseHandler = expense =>{
+    console.log('in app.js');
+    console.log(expenses)
+  }
   return (
-    <div>
-      <h2> lets start </h2>
+   /* <div>
+      
+      <h2>Let's get started!</h2>
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
         date={expenses[0].date}
-        ExpenseLocation={expenses[0].ExpenseLocation}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[1].title}
         amount={expenses[1].amount}
         date={expenses[1].date}
-        ExpenseLocation={expenses[1].ExpenseLocation}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[2].title}
         amount={expenses[2].amount}
         date={expenses[2].date}
-        ExpenseLocation={expenses[2].ExpenseLocation}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[3].title}
         amount={expenses[3].amount}
         date={expenses[3].date}
-        ExpenseLocation={expenses[3].ExpenseLocation}
-      ></ExpenseItem>
-    </div>
-  );
-}
+      />
+      */
+     <div>
+    <div>
 
+      <NewExpense onAddExpense={addExpenseHandler}/>
+      <Expenses items={expenses} />
+    </div>
+  
+   
+   </div>
+  )
+}
 export default App;
